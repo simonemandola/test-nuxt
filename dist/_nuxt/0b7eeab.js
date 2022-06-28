6123bfb3e24cacaaa2a61041697ea69d11d5969e
Simone <s.mandola.design@hotmail.com>
2022-06-28 15:42:48 +0200

    add dist

---
+++ dist/_nuxt/0b7eeab.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{249:function(e,n,t){"use strict";t.r(n);var l={name:"Welcome"},c=t(43),component=Object(c.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("¡Hola NuxtJS!")])}),[],!1,null,null,null);n.default=component.exports}}]);
