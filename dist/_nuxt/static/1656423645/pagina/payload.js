9bddfed57c0501fd67ff0bd28f8f8ff67a3e83e9
Simone <s.mandola.design@hotmail.com>
2022-06-28 16:18:18 +0200

    up

--- dist/_nuxt/static/1656423645/pagina/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/pagina", {data:[{}],fetch:{},mutations:void 0});
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,9 @@
 <template>
-  <div>
-    <Welcome />
-    <router-link :to="{name: 'pagina'}">Página de posts</router-link>
-  </div>
+  <Tutorial/>
 </template>
 
 <script>
-import Welcome from '@/components/Welcome'
 export default {
-  head() {
-    return {
-      title: "Página de inicio | CCSTech.io",
-      meta: [
-        {
-          hid: "description",
-          name: 'description',
-          content:"Sitio web de CCSTech.io"
-        },
-      ],
-      htmlAttrs: {
-        lang: "es",
-      },
-    };
-  },
-  name: 'index',
-  components: {
-    welcome: Welcome,
-  },
+  name: 'IndexPage'
 }
 </script>
