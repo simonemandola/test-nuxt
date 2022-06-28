6e91b097457867ac87cc337bbff7e32bca558083
Simone <s.mandola.design@hotmail.com>
2022-06-28 15:23:36 +0200

    add first page

---
+++ components/Welcome.vue
@@ -0,0 +1,10 @@
+<!-- Please remove this file from your project -->
+<template>
+  <h1>¡Hola NuxtJS!</h1>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
 <template>
-  <Tutorial />
+  <div>
+    <Welcome />
+    <router-link :to="{name: 'pagina'}">Página de posts</router-link>
+  </div>
 </template>
 
 <script>
+import Welcome from '@/components/Welcome'
 export default {
-  name: 'IndexPage',
+  head() {
+    return {
+      title: "Página de inicio | CCSTech.io",
+      meta: [
+        {
+          hid: "description",
+          name: 'description',
+          content:"Sitio web de CCSTech.io"
+        },
+      ],
+      htmlAttrs: {
+        lang: "es",
+      },
+    };
+  },
+  name: 'index',
+  components: {
+    welcome: Welcome,
+  },
 }
 </script>
